c3e024be0569badb95c2f3ea2ef9e667a92023c5
anushkadeshpande <anushkaa5000@gmail.com>
2023-12-17 04:39:36 +0530

    feat: Added lights to the scene

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,28 @@
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import Loader from '../components/Loader'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
 
 const Home = () => {
   return (
-    <section className='w-full h-screen relative'>
+    <section className="w-full h-screen relative">
       {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POP
       </div> */}
 
-      <Canvas className='w-full h-screen bg-transparent'
-      camera={{near: 0.1, far: 1000}}>
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
         <Suspense fallback={<Loader />}>
-          </Suspense>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight /> 
+          <hemisphereLight />
+        </Suspense>
       </Canvas>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
