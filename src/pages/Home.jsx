f3b580190a9c9b72968810480a34745ac683b7ce
anushkadeshpande <anushkaa5000@gmail.com>
2023-12-17 04:32:32 +0530

    feat: Addedd loader component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
 
 const Home = () => {
   return (
@@ -10,7 +11,8 @@ const Home = () => {
 
       <Canvas className='w-full h-screen bg-transparent'
       camera={{near: 0.1, far: 1000}}>
-
+        <Suspense fallback={<Loader />}>
+          </Suspense>
       </Canvas>
     </section>
   )
