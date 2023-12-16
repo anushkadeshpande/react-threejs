17e408ffd6d9e946e63be3f8f6648c67487e9b7c
anushkadeshpande <anushkaa5000@gmail.com>
2023-12-17 04:28:04 +0530

    config: Added react-three/fiber dependency and added a basic canvas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        POP
+      </div> */}
+
+      <Canvas className='w-full h-screen bg-transparent'
+      camera={{near: 0.1, far: 1000}}>
+
+      </Canvas>
+    </section>
   )
 }
 
