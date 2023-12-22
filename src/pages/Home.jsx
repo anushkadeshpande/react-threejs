77477fc0ef4bc95d42e95f3e358fb6b47af63093
anushkadeshpande <anushkaa5000@gmail.com>
2023-12-23 02:26:01 +0530

    feat: Added Sky component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,22 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
 
 const Home = () => {
-
   const adjustIslandForScreenSize = () => {
-    let screenScale = null
-    let screenPosition = [0, -6.5, -43]
-    let rotation = [0.1, 4.7, 0]
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
 
-    if(window.innerWidth < 768) 
-      screenScale = [0.9, 0.9, 0.9]
+    if (window.innerWidth < 768) screenScale = [0.9, 0.9, 0.9];
+    else screenScale = [1, 1, 1];
 
-    else 
-      screenScale = [1, 1, 1]
+    return [screenScale, screenPosition, rotation];
+  };
 
-      return [screenScale, screenPosition, rotation]
-  }
-
-  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
@@ -32,13 +30,18 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight position={[1,1,1]} intensity={2} />
-          <ambientLight intensity={0.5}/>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
           {/* <pointLight /> */}
           {/* <spotLight />  */}
-          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
 
-          <Island 
+          <Sky />
+          <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
