d6f80c29220e30438b11269a117a25e39fa7709c
anushkadeshpande <anushkaa5000@gmail.com>
2023-12-23 15:39:31 +0530

    feat: Added emailjs integration

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    emailjs.send(
+      import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Anna", 
+        from_email: form.email,
+        to_email: import.meta.env.VITE_MY_EMAIL,
+        message: form.message
+      },
+      import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false)
+      setForm({ name: "", email: "", message: "" })
+    }).catch(error => {
+      setIsLoading(false)
+      console.error(error)
+    });
+  };
 
   const handleFocus = () => {};
 
@@ -15,7 +42,11 @@ const Contact = () => {
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in touch</h1>
 
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+          ref={formRef}
+        >
           <label className="text-black-500 font-semibold">
             Name
             <input
@@ -66,7 +97,9 @@ const Contact = () => {
             className="btn"
             onFocus={handleFocus}
             onBlur={handleBlur}
-          >{isLoading? 'Sending...' : 'Send Message'}</button>
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
         </form>
       </div>
     </section>
