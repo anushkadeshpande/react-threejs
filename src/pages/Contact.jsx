9fc03bd6c70d1145a85384fa0d72ac3a748286b2
anushkadeshpande <anushkaa5000@gmail.com>
2023-12-23 14:57:22 +0530

    feat: Built contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
 
-export default Contact
+  const handleChange = () => {};
+
+  const handleFocus = () => {};
+
+  const handleBlur = () => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+
+        <form className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="Anna"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="aa@example.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Message
+            <textarea
+              name="message"
+              className="input"
+              placeholder="Your Message"
+              rows={4}
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >{isLoading? 'Sending...' : 'Send Message'}</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
